d2f8099d6aa0a742b5b0b93e74207423bc076ec6
carlosbandelliv@gmail.com <carlosbandelliv@gmail.com>
2021-09-26 18:07:48 -0300

    UseMemo

--- iniciando_hooks/src/App.js
+++ iniciando_hooks/src/App.js
@@ -1,25 +1,59 @@
-/* eslint-disable no-undef */
+/* eslint-disable react/jsx-no-comment-textnodes */
 import P from 'prop-types';
+import { useEffect, useMemo, useState } from 'react';
 import './App.css';
+
+const Post = ({ post }) => {
+  console.log('Filho renderizou');
+  return (
+    <div key={post.id} className="post">
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+    </div>
+  );
+};
+
+Post.propTypes = {
+  post: P.shape({
+    id: P.number,
+    title: P.string,
+    body: P.string,
+  }),
+};
 
 function App() {
   const [posts, setPosts] = useState([]);
-  console.log('Pai, renderizou!');
+  const [value, setValue] = useState('');
+
+  console.log('Pai renderizou!');
+
+  // Component did mount
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then((r) => r.json())
-      .then((r) => setPosts(r));
+    setTimeout(function () {
+      fetch('https://jsonplaceholder.typicode.com/posts')
+        .then((r) => r.json())
+        .then((r) => setPosts(r));
+    }, 5000);
   }, []);
+
   return (
     <div className="App">
-      {posts.map((post) => {
+      <p>
+        <input
+          type="search"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+      </p>
+      {useMemo(() => {
         return (
-          <div key={post.id} className="post">
-            <h1>{post.title}</h1>
-            <p>{post.body}</p>
-          </div>
+          posts.length > 0 &&
+          posts.map((post) => {
+            return <Post key={post.id} post={post} />;
+          })
         );
-      })}
+      }, [posts])}
+      {posts.length <= 0 && <p>Ainda não existem posts.</p>}
     </div>
   );
 }
